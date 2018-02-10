90c5876058c263892b47909dd7d882d39c48132b
siriustom <linxiaoran0210@gmail.com>
2018-02-09 22:09:20 -0800

    delete useless code

--- controllers/main.js
+++ controllers/main.js
@@ -1,35 +1,3 @@
-// var lineReader = require('line-reader');
-//
-// /**
-//  * Send the contents of an HTML page to the client.
-//  * @param fileName the name of the file containing the HTML page.
-//  * @param result the HTTP result.
-//  */
-// function sendPage(fileName, result)  {
-//     var html = '';
-//
-//     // Read the file one line at a time.
-//     lineReader.eachLine(fileName,
-//         /**
-//          * Append each line to string html.
-//          * Send the contents of html to the client
-//          * after the last line has been read.
-//          * @param line the line read from the file.
-//          * @param last set to true after the last line.
-//          */
-//         function(line, last) {
-//             html += line + '\n';
-//
-//             if (last) {
-//                 result.send(html);
-//                 return false;
-//             } else {
-//                 return true;
-//             }
-//         });
-// }
-
-
 /*
  * GET home page.
  */
