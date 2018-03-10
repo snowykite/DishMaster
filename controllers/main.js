2a88ed060d1191d449e18a6b1bdccb97ef15cb79
Mokuten OH <mokuteno@gmail.com>
2018-03-09 19:05:46 -0800

    redirect to home page after login

--- controllers/main.js
+++ controllers/main.js
@@ -14,63 +14,67 @@ module.exports.loggedIn = function (req, res, next) {
 /*
  * GET home page.
  */
-module.exports.home = function(request, response) {
-    // response.render will go to views directory to find module.
-    response.render('login', {});
+module.exports.home = function(req, res) {
+    if(!req.session.user)
+    {
+        // hasn't logged in
+        res.redirect('/login');
+    }
+    res.render('home', { email: req.session.user.email});
 }
 
 /*
  * GET login page.
  */
-module.exports.login = function(request, response) {
-    response.render('login', {title: 'Login'});
+module.exports.login = function(req, res) {
+    res.render('login', {title: 'Login'});
 }
 
 /*
  * POST login page.
  */
-module.exports.post_login = function(request, response) {
-    console.log(request.body);
+module.exports.post_login = function(req, res) {
+    console.log(req.body);
     var matches = registeredUsers.filter(function (user) {
-        return (user.email === request.body.email) &&
-                (user.password === request.body.password);
+        return (user.email === req.body.email) &&
+                (user.password === req.body.password);
     });
     if (matches.length === 0) {
-        response.render('login', { message: "invalid credentials!"});
+        res.render('login', { message: "invalid credentials!"});
     } else {
-        request.session.user = matches[0];
-        response.render('home', { email: request.session.user.email});
+        req.session.user = matches[0];
+        res.redirect('/home');
     }
 }
 
 /*
  * GET signup page.
  */
-module.exports.signup = function(request, response) {
-    response.render('signup', {title: 'Sign Up'});
+module.exports.signup = function(req, res) {
+    res.render('signup', {title: 'Sign Up'});
 }
 
 /*
  * POST signup page.
  */
-module.exports.post_signup = function(request, response) {
-    console.log(request.body);
-    if (!request.body.email || !request.body.password) {
-        response.status("400");
-        response.send("Invalid details.");
+module.exports.post_signup = function(req, res) {
+    console.log(req.body);
+    if (!req.body.email || !req.body.password) {
+        res.status("400");
+        res.send("Invalid details.");
     } else {
         var matches = registeredUsers.filter(function (user) {
-            return user.email === request.body.email;
+            return user.email === req.body.email;
         });
         if (matches.length > 0) {
-            response.render('signup', { message: "Already registered, please log in!"});
+            res.render('signup', { message: "Already registered, please log in!"});
         } else {
             var newUser = {
-                email: request.body.email,
-                password: request.body.password
+                email: req.body.email,
+                password: req.body.password
             };
             registeredUsers.push(newUser);
-            response.render('login', { message: "Please log in!"});
+            res.render('login', { message: "Please log in!"});
         }
     }
 }
