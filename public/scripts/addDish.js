f03ac1374afd17e28777eb693c15ff677e5aa21d
Mokuten OH <mokuteno@gmail.com>
2018-03-09 23:08:06 -0800

    display input value in confirm dialog

--- public/scripts/addDish.js
+++ public/scripts/addDish.js
@@ -1,12 +1,17 @@
 $(function(){
 	// drag and drop tags
+	var selectedTags = [];
 	$( ".dms-tag" ).draggable();
     $( "#selectedTags" ).droppable({
     	accept: ".dms-tag",	
       	drop: function( event, ui ) {
+      		selectedTags.push(ui.draggable.find("span").text());
+
         	$( this )
         	.children(".tags-container")
 			.append(ui.draggable.find("span"));
+
+
     	}
     });
 
@@ -20,6 +25,11 @@ $(function(){
     });
 
     $(document).on("click", "#confirmDishDetails", function(){
+    	$("#dishLabelConfirm").text(JSON.stringify(selectedTags));
+    	$("#dishNameConfirm").text($("#dishNameInput").val());
+    	$("#restaurantConfirm").text($("#restaurantInput").val());
+    	$("#dateConfirm").text($("#dateInput").val());
+    	
     	$( "#confirmDialog" ).dialog( "open" );
     });
 });
